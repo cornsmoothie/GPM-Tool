e353bec5ffad0bf7f5b444d54ba16458807d9fd4
Geo K <geok@Geos-MacBook-Pro.local>
2018-08-26 16:16:37 -0400

    Working file creations, bad implementation

--- passSetup.py
+++ passSetup.py
@@ -6,39 +6,42 @@ from cryptography.fernet import Fernet
 import getpass
 import os.path
 
-if __name__ == "__main__":
-    setAuth()
 
 def setAuth():
+
+    passPath = ("./Auth/pw")
+    keyPath = ("./Auth/key")
+
+    file_handle(passPath)
+    file_handle(keyPath)
+
     password = getpass.getpass('Password:')
 
     key = Fernet.generate_key()
     cipher_suite = Fernet(key)
     encryptedPassBytes = cipher_suite.encrypt(b'{password}')
 
-    passPath = Path("./Auth/pw")
-    keyPath = Path("./Auth/key")
-
-    file_handle(passPath)
-    file_handle(keyPath)
-
-    passFile = open("passPath" , "w")
-    keyFile = open("keyPath" , "w")
+    passFile = open(passPath , "wb")
+    keyFile = open(keyPath , "wb")
 
     passFile.write(encryptedPassBytes)
     keyFile.write(key)
 
 def file_handle(filename):
-    if not os.path.exists(os.path.dirname(filename)):
+    print(filename)
+    if not os.path.exists(filename):
         try:
-            os.makedirs(os.path.dirname(filename))
+            # os.makedirs(os.path.dirname(filename))
+            print("You're good")
         except OSError as exc: # Guard against race condition
-            if exc.errno != errno.EEXIST:
+            # if exc.errno != errno.EEXIST:
                 raise
     else:
-        print("File {filename} already exists, either use this file or delete it")
+        print(f"File {filename} already exists, either use this file or delete it")
         exit()
 
+if __name__ == "__main__":
+    setAuth()
     
 # if passPath.isfile():
 #     print("Password File already exists")
